bb2d0d7e258c67b812649fb782e4632a4731648b
anngreen <anngreen@qq.com>
2023-04-13 22:33:34 +0800

    数字马力

---
+++ 公司笔试题/数字马力/1.js
@@ -0,0 +1,23 @@
+// const rl = require("readline").createInterface({ input: process.stdin });
+// var iter = rl[Symbol.asyncIterator]();
+// const readline = async () => (await iter.next()).value;
+
+// void (async function () {
+//   // Write your code here
+//   let len = 0;
+//   let times = 0;
+//   let arr = [];
+
+//   line = await readline();
+//   let tokens = line.split(" ");
+//   len = parseInt(tokens[0]);
+//   times = parseInt(tokens[1]);
+
+//   console.log(len);
+//   console.log(times);
+// })();
+
+
+
+arr1.contact(arr2);
+console.log(arr1) 
